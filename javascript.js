9bf37091a97b337bf204be5962971e426fb43e15
Milton Tomé da Silva <miltontsilva@gmail.com>
2020-09-28 11:45:41 -0300

    Versão inicial

---
+++ javascript.js
@@ -0,0 +1,57 @@
+console.log('Armazenando apikey')
+
+let apikey = {key: 'f5b0779f-0772-487b-93d2-c7cc03e6e8c9' }
+
+//Get Fetch requisition
+
+fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' + apikey.key)
+    .then((response) => {
+        if(!response.ok)
+        {
+            throw new Error('Erro ao executar a requisição, status' + response.status);
+        }
+        else{
+            return response.json();
+        }
+    })
+    .then((api) => {
+        console.log(api)
+        var texto = "";
+
+        for ( let i = 0; i < 4; i++)
+        {
+            texto = texto + `
+
+            <div class="container">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Nome</th>
+                  <th>Simbolo</th>
+                  <th>Ultima Data</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td> <img src="coin.jpg" alt="coin" class="align-self-center mr-3" width="100" height="60"></td>
+                  <td>${api.data[i].name}</td>
+                  <td>${api.data[i].symbol}</td>
+                  <td>${api.data[i].last_historical_data}</td>
+                </tr>      
+               
+              </tbody>
+            </table>
+          </div>`
+
+               
+        }
+
+        document.getElementById("coins").innerHTML = texto;
+    })
+    .catch((error) => {
+        console.log(error.message);
+    }); 
+
+   
+
